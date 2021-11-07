f4f4e5f8bc7f36d877b37a732594042501367cd4
Denis Pershin <dyp@perchine.com>
2021-11-07 18:05:03 +0700

    Add djinni. Initial commit.

---
+++ cmake/djinni_process_idl.cmake
@@ -0,0 +1,131 @@
+include_guard(GLOBAL)
+set(djinni_cmake_utils_dir ${CMAKE_CURRENT_LIST_DIR} CACHE INTERNAL "")
+
+macro(djinni_process_idl)
+
+  set(options )
+  set(values DJINNI_IDL_FILE GENERATED_OUT_DIR VARIABLE_PREFIX PYTHON_PACKAGE NAMESPACE)
+  set(lists )
+
+  cmake_parse_arguments(_idldef
+      "${options}" "${values}" "${lists}" ${ARGN})
+
+  set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS
+      ${_idldef_DJINNI_IDL_FILE}
+  )
+
+  find_program(DJINNI_CMD djinni DOC "Path to djinni executable" REQUIRED)
+
+
+  get_filename_component(name_wle ${_idldef_DJINNI_IDL_FILE} NAME_WLE)
+  get_filename_component(name ${_idldef_DJINNI_IDL_FILE} NAME)
+  get_filename_component(name_real ${_idldef_DJINNI_IDL_FILE} REALPATH)
+
+  set(_idldef_LIST_OUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/cmake_${name}.generated.txt)
+  set(_idldef_CMAKE_FILE ${CMAKE_CURRENT_BINARY_DIR}/${name}.cmake)
+
+  set(skip_djinni_call OFF)
+  if(EXISTS ${_idldef_LIST_OUT_FILE})
+      if(NOT ${name_real} IS_NEWER_THAN ${_idldef_LIST_OUT_FILE})
+          message("-- djiini call not required, already processed ${name}.")
+          set(skip_djinni_call ON)
+      endif()
+  endif()
+
+  if(NOT skip_djinni_call)
+    string(REPLACE "::" "/" namespace_path ${_idldef_NAMESPACE})
+
+    set(LIST_FILE ${_idldef_LIST_OUT_FILE})
+    set(OUT_FILE ${_idldef_CMAKE_FILE})
+    set(VARIABLE_PREFIX ${_idldef_VARIABLE_PREFIX})
+
+    set(arg_py_out "../${_idldef_GENERATED_OUT_DIR}/${namespace_path}/py/")
+    set(arg_pycffi_out "../${_idldef_GENERATED_OUT_DIR}/${namespace_path}/pycffi/")
+    set(arg_cwrapper_out "../${_idldef_GENERATED_OUT_DIR}/${namespace_path}/c-wrapper/")
+    set(arg_cwrapper_header_out "../${_idldef_GENERATED_OUT_DIR}/include/${namespace_path}/c-wrapper")
+
+    set(arg_cpp_out "../${_idldef_GENERATED_OUT_DIR}/${namespace_path}/cpp/")
+    set(arg_cpp_header_out "../${_idldef_GENERATED_OUT_DIR}/include/${namespace_path}/cpp")
+    set(arg_yaml_out "../${_idldef_GENERATED_OUT_DIR}/${namespace_path}/yaml/${name}.yaml")
+
+    set(djinni_args
+        --idl "../${_idldef_DJINNI_IDL_FILE}"
+        --py-out ${arg_py_out}
+        --pycffi-out ${arg_pycffi_out}
+        --pycffi-package-name ${_idldef_PYTHON_PACKAGE}
+        --c-wrapper-out ${arg_cwrapper_out}
+        --c-wrapper-header-out ${arg_cwrapper_header_out}
+        --cpp-out ${arg_cpp_out}
+        --cpp-header-out ${arg_cpp_header_out}
+        --cpp-include-prefix ${namespace_path}/cpp/
+        --cpp-namespace ${_idldef_NAMESPACE}
+        --yaml-out ${arg_yaml_out}
+        --list-out-files ${_idldef_LIST_OUT_FILE}
+        --ident-cpp-file FooBar
+    )
+
+    message("Generating djinni source files for ${name}")
+    message("${DJINNI_CMD} ${djinni_args}")
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_CURRENT_LIST_DIR}/${_idldef_GENERATED_OUT_DIR}
+      COMMAND ${DJINNI_CMD} ${djinni_args}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+     # COMMAND_ERROR_IS_FATAL LAST
+      #COMMAND_ECHO STDOUT
+    )
+
+    message("Generating djinni cmake include file for ${name}")
+    FILE(READ "${LIST_FILE}" generated_files)
+    STRING(REGEX REPLACE ";" "\\\\;" generated_files "${generated_files}")
+    STRING(REGEX REPLACE "\n" ";" generated_files "${generated_files}")
+
+    set(CPP_SOURCE "")
+    set(CPP_HEADER "")
+    set(CWRAPPER_SOURCE "")
+    set(CWRAPPER_HEADER "")
+    set(PY_SOURCE "")
+    set(PYCFFI_SOURCE "")
+
+    foreach(item ${generated_files})
+      if(item MATCHES "^${arg_cpp_out}")
+        list(APPEND CPP_SOURCE ${item})
+      elseif(item MATCHES "^${arg_cpp_header_out}")
+        list(APPEND CPP_HEADER ${item})
+      elseif(item MATCHES "^${arg_cwrapper_out}")
+        list(APPEND CWRAPPER_SOURCE ${item})
+      elseif(item MATCHES "^${arg_cwrapper_header_out}")
+        list(APPEND CWRAPPER_HEADER ${item})
+      elseif(item MATCHES "^${arg_cpp_header_out}")
+        list(APPEND CPP_HEADER ${item})
+      elseif(item MATCHES "^${arg_py_out}")
+        list(APPEND PY_SOURCE ${item})
+      elseif(item MATCHES "^${arg_pycffi_out}")
+        list(APPEND PYCFFI_SOURCE ${item})
+      elseif(item MATCHES "^${arg_yaml_out}")
+        # ignore
+      else()
+        message(WARNING "Unhandled generated file: " ${item})
+      endif()
+
+    endforeach()
+
+    string (REPLACE ";" " " CPP_SOURCE "${CPP_SOURCE}")
+    string (REPLACE ";" " " CPP_HEADER "${CPP_HEADER}")
+    string (REPLACE ";" " " CWRAPPER_SOURCE "${CWRAPPER_SOURCE}")
+    string (REPLACE ";" " " CWRAPPER_HEADER "${CWRAPPER_HEADER}")
+    string (REPLACE ";" " " PY_SOURCE "${PY_SOURCE}")
+    string (REPLACE ";" " " PYCFFI_SOURCE "${PYCFFI_SOURCE}")
+
+    string (REPLACE "../" "" CPP_SOURCE "${CPP_SOURCE}")
+    string (REPLACE "../" "" CPP_HEADER "${CPP_HEADER}")
+    string (REPLACE "../" "" CWRAPPER_SOURCE "${CWRAPPER_SOURCE}")
+    string (REPLACE "../" "" CWRAPPER_HEADER "${CWRAPPER_HEADER}")
+    string (REPLACE "../" "" PY_SOURCE "${PY_SOURCE}")
+    string (REPLACE "../" "" PYCFFI_SOURCE "${PYCFFI_SOURCE}")
+
+    configure_file(${djinni_cmake_utils_dir}/djinni_generated_files.cmake.in
+                  ${OUT_FILE}
+                  @ONLY
+    )
+  endif()
+endmacro()
--- setup.py
+++ setup.py
@@ -1,5 +1,87 @@
-from setuptools import setup, find_packages
+import os
+import platform
+import subprocess
+import sys
+from setuptools import setup, find_packages, Extension
 from os.path import join, dirname
+from setuptools.command.build_ext import build_ext
+from pprint import pprint
+
+# Filename for the C extension module library
+c_module_name = '_native'
+
+# Command line flags forwarded to CMake (for debug purpose)
+cmake_cmd_args = []
+for f in sys.argv:
+    if f.startswith('-D'):
+        cmake_cmd_args.append(f)
+
+for f in cmake_cmd_args:
+    sys.argv.remove(f)
+
+
+def _get_env_variable(name, default='OFF'):
+    if name not in os.environ.keys():
+        return default
+    return os.environ[name]
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, cmake_lists_dir='.', sources=[], **kwa):
+        Extension.__init__(self, name, sources=sources, **kwa)
+        self.cmake_lists_dir = os.path.abspath(cmake_lists_dir)
+
+
+class CMakeBuild(build_ext):
+    def build_extensions(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError('Cannot find CMake executable')
+
+        for ext in self.extensions:
+
+            extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+            cfg = 'Debug' if _get_env_variable('DISPTOOLS_DEBUG') == 'ON' else 'Release'
+
+            cmake_args = [
+                '-DCMAKE_BUILD_TYPE=%s' % cfg,
+                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir),
+                '-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), self.build_temp),
+                '-DDJINNI_WITH_PYTHON=ON',
+                '-DDJINNI_WITH_OBJC=OFF',
+                '-DDJINNI_WITH_JNI=OFF',
+                '-DDJINNI_BUILD_TESTING=OFF',
+            ]
+
+            if platform.system() == 'Windows':
+                plat = ('x64' if platform.architecture()[0] == '64bit' else 'Win32')
+                cmake_args += [
+                    '-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE',
+                    '-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir),
+                ]
+                if self.compiler.compiler_type == 'msvc':
+                    cmake_args += [
+                        '-DCMAKE_GENERATOR_PLATFORM=%s' % plat,
+                    ]
+                else:
+                    cmake_args += [
+                        '-G', 'MinGW Makefiles',
+                    ]
+
+            cmake_args += cmake_cmd_args
+
+            pprint(cmake_args)
+
+            if not os.path.exists(self.build_temp):
+                os.makedirs(self.build_temp)
+
+            # Config and build the extension
+            subprocess.check_call(['cmake', ext.cmake_lists_dir] + cmake_args,
+                                  cwd=self.build_temp)
+            subprocess.check_call(['cmake', '--build', '.', '--config', cfg, '--', '-j'],
+                                  cwd=self.build_temp)
+
 
 all_packages = []
 for pkg in find_packages():
@@ -18,9 +100,11 @@ setup(
     packages=all_packages,
     package_dir={'ocelot.demos': 'demos'},  ## install examples along with the rest of the source
     install_requires=[
-        'numpy', 'scipy', 'matplotlib', 'pandas'
+        'numpy', 'scipy', 'matplotlib', 'pandas', 'cffi', 'future'
     ],
     extras_require={'docs': ['Sphinx', 'alabaster', 'sphinxcontrib-jsmath']},
+    ext_modules=[CMakeExtension(c_module_name)],
+    cmdclass={'build_ext': CMakeBuild},
     package_data={'ocelot.optics': ['data/*.dat']},
     license="GNU General Public License v3.0",
 )
